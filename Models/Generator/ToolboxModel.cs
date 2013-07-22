aae1ae6a167011439c022d3a2edcc8f9a314cfb8
wrathal <wrathal00@gmail.com>
2013-07-22 21:39:20 +0200

    Poczatkowa solucja

---
+++ Models/Generator/ToolboxModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using KendoGenerator.Models.Controls;
+
+namespace KendoGenerator.Models.Generator
+{
+    public class ToolboxModel
+    {
+        public List<IBaseControlModel> Controls { get; set; }
+    }
+}
